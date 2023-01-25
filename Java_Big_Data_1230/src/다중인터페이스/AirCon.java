cc81ce21c88b118b13d6d0c7c17c04e80c57403a
cjiin <wldlsdl915@naver.com>
2023-01-25 15:08:18 +0900

    1월 25일 객체

---
+++ Java_Big_Data_1230/src/다중인터페이스/AirCon.java
@@ -0,0 +1,10 @@
+package 다중인터페이스;
+
+public interface AirCon {
+    int MAX_TEMP = 30; // public final static 이 자동으로 추가됨(컴파일러)
+    int MIN_TEMP = 0;
+    void airConON(); // 자동으로 추상메소드가 됨 ( abstract 자동 추가 됨)
+    void airConOFF();
+    void setAirConTemp(int temp);
+
+}
