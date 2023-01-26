81100768c6615ae9fd7704e201ff57a4dd0f2dd4
cjiin <wldlsdl915@naver.com>
2023-01-26 15:06:22 +0900

    1월 26일 알고리즘

---
+++ Java_Big_Data_1230/src/단어공부/WordStudy.java
@@ -0,0 +1,21 @@
+package 단어공부;
+// 알파벳 대소문자로 이루어진 단어가 주어지면 가장 많이 등장하는 알파벳을 찾는 문제
+// 입력은 대소문자를 입력하고, 결과는 대문자로 출력
+// 만약 가장 많이 등장하는 알파벳이 여러개인 경우 ?로 출력
+// 결과 예시 : AaAbcdd => A
+// 결과 예시 : ddcfgrG => ?
+
+import java.util.Scanner;
+
+public class WordStudy {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        for (int i = 0; i<str.length();i++) {
+
+        }
+
+    }
+
+}
