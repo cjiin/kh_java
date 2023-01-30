69dd28d52e487d451b31c1b71a8d4563bf224006
cjiin <wldlsdl915@naver.com>
2023-01-30 15:09:48 +0900

    1월 30일 ArrayList

---
+++ Java_Big_Data_1230/src/ArrayList객체/ArrayListObject.java
@@ -0,0 +1,18 @@
+package ArrayList객체;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListObject {
+    public static void main(String[] args) {
+        List<MenuInfo> menuList = new ArrayList<>();
+        menuList.add(new MenuInfo("Americano",2500,"Coffee", "기본커피입니다."));
+        menuList.add(new MenuInfo("Latte",4500,"Coffee", "우유가 포함된 커피입니다."));
+        MenuInfo test = new MenuInfo("Espresso",2500,"Coffee","진한커피 dlqslek.");
+        menuList.add(test);
+
+        for(MenuInfo e : menuList) {
+            e.getMenuInfo();
+        }
+    }
+}
