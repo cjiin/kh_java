78e64518a86e29a4f92ed9b15e5cba87580c085d
cjiin <wldlsdl915@naver.com>
2023-01-31 22:23:40 +0900

    연동 테스트

--- Java_Big_Data_1230/src/HashSet응용/HashSetApplyEx.java
+++ Java_Big_Data_1230/src/HashSet응용/HashSetApplyEx.java
@@ -1,7 +1,7 @@
 package HashSet응용;
 // 문제 : 알파벳 소문자로 이루어진 N개의 단어가 들어오면 아래와 같은 조건에 따라 정렬하는 프로그램 작성
 // 1. 길이가 짧은 것 부터
-// 2. 길이가 같으면 사전 순으로
+// 2. 길이가 같으면 사전 순으로 정렬
 // 3. 중복 제거
 // 입력 : 13 but i wont hesitate no more no more it cannot wait im yours
 // 출력 : i im it no but more wait wont yours cannot hesitate
