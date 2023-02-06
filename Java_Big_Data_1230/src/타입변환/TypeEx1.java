04f2ba3f8898082eee8a6dcfd43cb3d43d177914
cjiin <wldlsdl915@naver.com>
2023-02-06 14:45:31 +0900

    2월 6일

--- Java_Big_Data_1230/src/타입변환/TypeEx1.java
+++ Java_Big_Data_1230/src/타입변환/TypeEx1.java
@@ -1,24 +1,24 @@
 //ckage 타입변환;
 //타입변환이란? 하나의 타입을 다른 타입으로 변경 하는 것
 
-import javax.swing.plaf.synth.SynthOptionPaneUI;
 
-//public class TypeEx1 {
-//    public static void main(String[] args) {
-//        int num1 = 1, num2 = 4;
-//        double rst1 = num1/ num2; // 문제가 발생하는 코드
-//        double rst2 = (double)num1/ num2; // 명시적 형변환과 묵시적 형변환이 일어남
-//        double rst3 = 100 + 100.1;
-//        System.out.println(rst1);
-//        System.out.println(rst2);
-//
-//        String phoneNumber = "010";
-//        String phoneNumber1 = "3.14";
-//        String name = "곰돌이사육사";
-//
-//        int number = Integer.parseInt(phoneNumber);
-//        System.out.println(number);
-//
-//        double number1 = Double.parseDouble(phoneNumber1);
-//        System.out.println(number1);
+public class TypeEx1 {
+    public static void main(String[] args) {
+        int num1 = 1, num2 = 4;
+        double rst1 = num1 / num2; // 문제가 발생하는 코드
+        double rst2 = (double) num1 / num2; // 명시적 형변환과 묵시적 형변환이 일어남
+        double rst3 = 100 + 100.1;
+        System.out.println(rst1);
+        System.out.println(rst2);
 
+        String phoneNumber = "010";
+        String phoneNumber1 = "3.14";
+        String name = "곰돌이사육사";
+
+        int number = Integer.parseInt(phoneNumber);
+        System.out.println(number);
+
+        double number1 = Double.parseDouble(phoneNumber1);
+        System.out.println(number1);
+    }
+}
