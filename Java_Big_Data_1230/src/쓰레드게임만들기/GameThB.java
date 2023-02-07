0f151e1acffeaab3fd0dd1099370343e4eb57e19
cjiin <wldlsdl915@naver.com>
2023-02-07 20:13:15 +0900

    2월 7일 쓰레드게임만들기예제

---
+++ Java_Big_Data_1230/src/쓰레드게임만들기/GameThB.java
@@ -0,0 +1,39 @@
+package 쓰레드게임만들기;
+
+public class GameThB extends Thread {
+        private Character warrior; //  외부에서 만들어진 객체를 주입받음. 클래스에 대한 참조변수를 만듦
+        private Character wizard;
+
+        public GameThB(Character warrior, Character wizard) {
+            this.warrior = warrior; // 전사
+            this.wizard = wizard; // 마법사
+        }
+    @Override
+    public  void run() {
+        int normal;
+        int special; // 특수 공격에 대한 확률 구하기
+        boolean endGame = false; // 게임에 대한 종료 여부
+        while (true) {
+            try {
+                sleep(3300); // 3초마다 반복
+                normal = (int)(Math.random() * 2); //50% 확률
+                special =(int)(Math.random() * 18); // 1/18 확률
+                if(normal == 1) {
+                    System.out.println("마법 공격 : " + warrior.name + "에게 " + wizard.mAttack()+ " 데미지를 입혔습니다.");
+                    endGame = warrior.setDamage(wizard.pAttack());
+                } else {
+                    System.out.println("물리 공격 : " + warrior.name + "에게 " + wizard.pAttack()+ " 데미지를 입혔습니다.");
+                    endGame = warrior.setDamage(wizard.mAttack());
+                }
+                if(special == 10) { // 1/18 확률
+                    System.out.println(wizard.name + " 궁극기 발동 !!!, " + warrior.name + "에게 " + wizard.ultimate() + " 데미지를 입혔습니다.");
+                    endGame = warrior.setDamage(wizard.ultimate());
+                }
+                if(endGame) {
+                    System.exit(0);
+                }
+            }catch (InterruptedException e ) {}
+        }
+    }
+}
+
