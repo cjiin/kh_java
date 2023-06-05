e89490b1f913ce61bdc56f7cb7309b9c514c944e
cjiin <wldlsdl915@naver.com>
2023-06-05 12:34:20 +0900

    6월 5일 자바 복습

---
+++ Java_Big_Data_1230/src/다형성복습/Vehicle.java
@@ -0,0 +1,26 @@
+package 다형성복습;
+
+public class Vehicle {
+    public void run() {
+        System.out.println("차량이 달립니다.");
+    }
+}
+
+class Bus extends Vehicle {
+    @Override
+    public void run() {
+        System.out.println("버스가 달립니다.");
+    }
+}
+class Taxi extends Vehicle {
+        @Override
+    public void run() {
+            System.out.println("택시가 달립니다.");
+        }
+    }
+class SportCar extends Vehicle {
+    @Override
+    public void run() {
+        System.out.println("승용차가 달립니다.");
+    }
+}
---
+++ Java_Big_Data_1230/src/인터페이스복습1번/CocoaTalkMain.java
@@ -0,0 +1,17 @@
+package 인터페이스복습1번;
+
+import java.util.Scanner;
+
+public class CocoaTalkMain {
+    public static void main(String[] args) {
+        CocoaTalk cocoaTalk = new CocoaTalk("곰돌이사육사");
+        cocoaTalk.writeMsg("벌써 여름이 왔어요. 잘 지내시죠?");
+        NetAdapter adapter;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("네트워크 1 WiFi 2 5G : ");
+        int sel = sc.nextInt();
+        if(sel == 1) adapter = new WiFi();
+        else  adapter = new FiveG();
+        cocoaTalk.send(adapter);
+    }
+}
